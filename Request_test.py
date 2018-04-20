41edc5f0fc893263a5bb7da703232a24e46574f6
Jemmy Liu <27750910@qq.com>
2018-04-20 18:00:09 +0800

    update

--- Request_test.py
+++ Request_test.py
@@ -1,4 +1,23 @@
+import re
 import requests
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 res=requests.get('http://www.baidu.com')
+file = open('html.html', 'w' )
+file.write(res.text)
 res.encoding='utf-8'
-print(res.text)
+#print(res.text)
+
+baidu = re.findall('<title>(.*?)</title>', res.text)
+
+for i in baidu:
+    print(i)
+
+print(baidu[0])
+
+
+
